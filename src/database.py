bf0ec90042c552d4de904701b0c939f2a9d388eb
Johann <johann12032005@gmail.com>
2024-10-23 18:33:24 -0600

    Correciones en la conexion a la base de datos

--- src/database.py
+++ src/database.py
@@ -1,11 +1,14 @@
 import mysql.connector
 
-
+# Conexión a la base de datos
 database = mysql.connector.connect(
-    host='localhost',          
-    database='crud' 
+    host='localhost',           # localhost para conexión local
+    user='root',                # Cambia 'root' por tu usuario de MySQL
+    password='',                # Cambia por tu contraseña, si es que tienes
+    database='crud' # Reemplaza 'tu_base_de_datos' con el nombre de tu base de datos
 )
 
+# Verificación de conexión
 if database.is_connected():
     print("Conexión exitosa a la base de datos")
 else:
